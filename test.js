bb421adfcaddbb0fa275055803e1a2b4b6c78c45
campan43 <campan43@gmail.com>
2013-10-15 23:39:58 +0200

    Title edited

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 Konami.imagePath = 'http://github.com/jmhobbs/Konami-Unicorn-Blitz/raw/master/';
 $(".ilMainMenuHeadSection img").attr("src", "http://userserve-ak.last.fm/serve/_/62660201/NONSTOP+NYAN+CAT+anigif_nyancatgif3211113033150.gif");
+$(".ilTopTitle").html("Nyancat rocks!");
 console.log(123);
 });
