163a0b76e42635c8d93b6795c0f09dfc50a46d59
campan43 <campan43@gmail.com>
2013-10-15 23:35:09 +0200

    Nyancat added

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
 Konami.imagePath = 'http://github.com/jmhobbs/Konami-Unicorn-Blitz/raw/master/';
+$(".ilMainMenuHeadSection img").attr("src", "http://userserve-ak.last.fm/serve/_/62660201/NONSTOP+NYAN+CAT+anigif_nyancatgif3211113033150.gif");
 console.log(123);
 });
